33d1b64e43efaa621c4137a12c3a82014facd9aa
Erik van der Schaaf <erik.17021995@gmail.com>
2018-09-20 13:21:11 +0200

    did a bit of css work

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+	<title>Simple RSS feed</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<?php include 'rss.php';?>
+	</div>
+</body>
+</html>
---
+++ rss.php
@@ -0,0 +1,35 @@
+<?php
+
+  $xml = 'https://www.volkskrant.nl/sport/rss.xml'; 
+  $xmlDoc = new DOMDocument();
+  $xmlDoc->load($xml);
+
+  //get elements from "<channel>"
+  $channel=$xmlDoc->getElementsByTagName('channel')->item(0);
+  $channel_title = $channel->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+  $channel_link = $channel->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+  $channel_desc = $channel->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
+  $image_src = $xmlDoc->getElementsByTagName('image')->item(0);
+  $channel_image = $image_src->getElementsByTagName('url')->item(0)->childNodes->item(0)->nodeValue;
+
+  //output elements from "<channel>"
+  $output = "";
+  $output .= "<div class='channel'><img src='" . $channel_image . "' width='140px' height='140px'>";
+  $output .= "<h2><a target='_blank' href='" . $channel_link. "'>" . $channel_title . "</a></h2>";
+  $output .= "<h3>" . $channel_desc . "</h3>";
+  $output .= "</div>";
+
+  //get and output "<item>" elements
+  $x=$xmlDoc->getElementsByTagName('item');
+  for ($i=0; $i<=10; $i++) {
+    $item_title=$x->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+    $item_link=$x->item($i)->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+    $item_desc=$x->item($i)->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
+    $output .= "<div class='item'>";
+    $output .= "<h3 class='title'><a target='_blank' href='" . $item_link . "'>" . $item_title . "</a></h3>";
+    $output .= $item_desc."...";
+    $output .= "<br>";
+    $output .= "<a class='button' target='_blank' href='" . $item_link . "'>Lees Meer</a></div>";
+  }
+  echo $output;
+?>
