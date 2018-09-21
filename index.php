c33c34097f9e020ff32d3dedba62f195e0b8afe3
Erik van der Schaaf <erik.17021995@gmail.com>
2018-09-21 14:51:51 +0200

    Added multiple themes you can change to

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
 	<title>Simple RSS feed</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
@@ -8,5 +9,21 @@
 	<div class="container">
 		<?php include 'rss.php';?>
 	</div>
+	<div id="toggle_editor">
+		<span class="arrow_left"><i class="fas fa-angle-left"></i></span>
+		<span class="arrow_right"><i class="fas fa-angle-right"></i></span>
+		<span class="cog"><i class="fas fa-cog"></i></span>
+	</div>
+	<div id="editor">
+		<span class="close"><i class="fas fa-times"></i></span>
+		<h2>Select theme</h2>
+		<div class="themes">
+			<ul>
+				<li class="theme" id="default">Default</li>
+			</ul>
+		</div>
+	</div>
 </body>
 </html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
---
+++ js/custom.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+	$('.arrow_left').click(function(){
+		$('.arrow_left').hide();
+		$('#toggle_editor').animate({
+			width: '280px'
+		});
+		$('.arrow_right').show();
+	});
+
+	$('.arrow_right').click(function(){
+		$('.arrow_right').hide();
+		$('#toggle_editor').animate({
+			width: '200px'
+		});
+		$('.arrow_left').show();
+	});
+
+	$('.cog').click(function(){
+		//$('#toggle_editor').hide();
+		$('#editor').slideDown();
+	});
+
+	$('.close').click(function(){
+		$('#editor').slideUp();
+		//$('#toggle_editor').show();
+	});
+
+	var themes = ['red', 'blue', 'orange'];
+
+	$.each(themes, function(i, val){
+		switch(val) {
+			case 'red':
+				var color = '114, 34, 61';
+				var trigger_point = 1300;
+				break;
+			case 'blue':
+				var color = '62, 79, 129';
+				var trigger_point = 1100;
+				break;
+			case 'orange':
+				var color = '249, 115, 62';
+				var trigger_point = 1500;
+				break;
+		}
+		var end_point = trigger_point + 200; 
+		$('.themes ul').append('<li class="theme" id="'+val+'">'+val+'</li>');
+		$('#'+val).click(function(){
+			$('body').css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba('+color+', 0.65) '+trigger_point+'px, rgba('+color+', 1) '+end_point+'px), url("img/background_'+val+'.jpg") no-repeat').css('background-size', 'contain');
+			$('.button').css('background-color', 'rgb('+color+')');
+		});
+	});
+
+	$('#default').click(function(){
+		$('body').css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(165, 126, 80, 0.65) 1300px, rgba(165, 126, 80, 1) 1500px), url("img/background.jpg") no-repeat').css('background-size', 'contain');
+		$('.button').css('background-color', '#a57e50');
+	});
+
+
+});
+
